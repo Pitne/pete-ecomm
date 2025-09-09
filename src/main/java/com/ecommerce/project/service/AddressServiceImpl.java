c65510c65f024367c770ef098cf1a885f0853a52
Pitne <lamminhkhoi2020@gmail.com>
2025-09-09 14:53:49 +1000

    feat(app): Create a API for Address: Create, Get address, Get address by id, Get user addresses, Update and Delete

---
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -0,0 +1,92 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.repositories.AddressRepository;
+import com.ecommerce.project.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+        Address address = modelMapper.map(addressDTO, Address.class);
+        List<Address> addressList = user.getAddresses();
+        addressList.add(address);
+        user.setAddresses(addressList);
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(savedAddress, AddressDTO.class);
+    }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addresses = addressRepository.findAll();
+        List<AddressDTO> addressDTOS = addresses.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .toList();
+        return addressDTOS;
+    }
+
+    @Override
+    public AddressDTO getAddressesById(Long addressId) {
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+        return modelMapper.map(address, AddressDTO.class);
+    }
+
+    @Override
+    public List<AddressDTO> getUserAddresses(User user) {
+        List<Address> addresses = user.getAddresses();
+        List<AddressDTO> addressDTOS = addresses.stream()
+                .map(address -> modelMapper.map(address, AddressDTO.class))
+                .toList();
+        return addressDTOS;
+    }
+
+    @Override
+    public AddressDTO updateAddress(Long addressId, AddressDTO addressDTO) {
+        Address addressFromDatabase = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+        addressFromDatabase.setCityName(addressDTO.getCityName());
+        addressFromDatabase.setCountry(addressDTO.getCountry());
+        addressFromDatabase.setPostalCode(addressDTO.getPostalCode());
+        addressFromDatabase.setState(addressDTO.getState());
+        addressFromDatabase.setStreet(addressDTO.getStreet());
+        addressFromDatabase.setBuildingName(addressDTO.getBuildingName());
+        Address updatedAddress = addressRepository.save(addressFromDatabase);
+        User user = addressFromDatabase.getUser();
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        user.getAddresses().add(updatedAddress);
+        userRepository.save(user);
+        return modelMapper.map(updatedAddress, AddressDTO.class);
+    }
+
+    @Override
+    public String deleteAddress(Long addressId) {
+        Address addressFromDatabase = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+        User user = addressFromDatabase.getUser();
+        user.getAddresses().removeIf(address -> address.getAddressId().equals(addressId));
+        userRepository.save(user);
+        addressRepository.delete(addressFromDatabase);
+        return "Address has been deleted: " + addressId;
+    }
+}
