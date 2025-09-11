37b7b7b116a0181ccd3e105ae7e1e53a2f197397
Pitne <lamminhkhoi2020@gmail.com>
2025-09-11 13:02:01 +1000

    fix(app): remove all import not in use

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -5,10 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "addresses")
